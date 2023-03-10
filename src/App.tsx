b013c2f9dbf8456c2db6e36279124c127d206596
sbo69976 <sbo69976@edu.srshb.ru>
2023-03-10 17:08:47 +0600

    add base editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { ViewUpdate } from '@codemirror/view';
+import { dracula } from "@uiw/codemirror-theme-dracula"
 
 function App() {
+  const [code, setCode] = useState<string | undefined>(undefined)
+  const onChange = React.useCallback((value: string, viewUpdate: ViewUpdate) => {
+    setCode(value)
+  }, []);
+
+  const runCode = () => {
+    if (code) {
+      const newFunc = new Function(code)
+      console.log(newFunc())
+    }
+  }
   return (
-    <div></div>
+    <>
+      <CodeMirror
+        value="console.log('hello world!');"
+        height="400px"
+        width="400px"
+        extensions={[javascript({ jsx: true })]}
+        onChange={onChange}
+        theme={dracula}
+      />
+      <button onClick={runCode}>run</button>
+    </>
   );
 }
-
 export default App;
